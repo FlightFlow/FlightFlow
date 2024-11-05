f792ad68f566ce9fc885e65f3c17c7464930023e
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-11-05 14:39:19 +0300

    Fixed run.sh script

--- .scripts/run.sh
+++ .scripts/run.sh
@@ -5,8 +5,10 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 FRONTEND_PATH="$SCRIPT_DIR/../frontend"
 BACKEND_PATH="$SCRIPT_DIR/../backend"
 
+echo "$BACKEND_PATH"
+
 pnpm concurrently -k \
   -n "frontend,backend" \
   -c "bgBlue.bold,bgGreen.bold" \
-  "cd $FRONTEND_PATH pnpm run dev" \
-  "cd $BACKEND_PATH && source $BACKEND_PATH/.venv/bin/activate && uvicorn main:app --host 0.0.0.0 --port 8000 --reload"
+  "cd $FRONTEND_PATH && pnpm dev" \
+  "bash -c 'cd $BACKEND_PATH/app && source ../../backend/.venv/bin/activate && uvicorn main:app --host 0.0.0.0 --port 8000 --reload'"
