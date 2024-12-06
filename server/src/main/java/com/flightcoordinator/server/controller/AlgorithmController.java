0fe01d6338761c6c60b7bb5ca8da5c9f0dd4f323
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-06 19:06:27 +0300

    Added algorithm related rest components

---
+++ server/src/main/java/com/flightcoordinator/server/controller/AlgorithmController.java
@@ -0,0 +1,18 @@
+package com.flightcoordinator.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flightcoordinator.server.service.AlgorithmService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/algorithm")
+@Tag(name = "Algorithm Controller", description = "Endpoint for managing automation algorithms.")
+public class AlgorithmController {
+  @Autowired
+  private AlgorithmService service;
+
+}
---
+++ server/src/main/java/com/flightcoordinator/server/service/AlgorithmService.java
@@ -0,0 +1,30 @@
+package com.flightcoordinator.server.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flightcoordinator.server.repository.AlgorithmRepository;
+
+@Service
+public class AlgorithmService {
+  @Autowired
+  private AlgorithmRepository repository;
+
+  public void triggerAllAlgorithm() {
+  }
+
+  public void triggerAlgorithmByName(String algorithmName) {
+  }
+
+  public void getAlgorithmRunsById(String runId) {
+  }
+
+  public void getAllAlgorithmRuns() {
+  }
+
+  public void getAlgorithmResultsById(String runIdw) {
+  }
+
+  public void getAllAlgorithmResults() {
+  }
+}
