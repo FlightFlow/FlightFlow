37aab17ecf2ec57e6f395569ee64bbd09bcc57f7
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-06 12:50:27 +0300

    Updated config scripts

--- .scripts/config/install_apps.sh
+++ .scripts/config/install_apps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # NVM AND NODE
-echo "Info: Installing Node Version Manager"
+echo "Info: Installing Node Version Manager..."
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 nvm install 22
 
-echo "Info: Succesfully installed Node Version Manager"
+echo "Info: Succesfully installed Node Version Manager."
 
 # PNPM
 echo "Info: Installing pnpm..."
@@ -14,3 +14,23 @@ echo "Info: Installing pnpm..."
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 
 echo "Info: Successfully installed pnpm."
+
+# MAVEN
+echo "Info: Installing Maven..."
+
+if ! grep -q 'export PATH=$MAVEN_HOME/bin:$PATH' ~/.bashrc; then
+  cd /tmp
+  wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+  sudo tar -xvzf apache-maven-3.9.9-bin.tar.gz -C /opt
+  sudo rm -rf apache-maven-3.9.9-bin.tar.gz
+
+  echo "" >> ~/.bashrc
+  echo "export MAVEN_HOME=/opt/apache-maven-3.9.9" >> ~/.bashrc
+  echo 'PATH=$MAVEN_HOME/bin:$PATH' >> ~/.bashrc
+
+  source ~/.bashrc
+
+  echo "Info: Successfully installed Maven."
+else
+  echo "Info: Maven environment variables found in ~./bashrc. Skipping..."
+fi
--- .scripts/config/setup_project.sh
+++ .scripts/config/setup_project.sh
@@ -9,13 +9,13 @@ BACKEND_PATH="$SCRIPT_DIR/../server"
 # PRE-COMMIT HOOKS
 echo "Info: Setting up pre-commit hook..."
 
-ln -sf ../../.githooks/pre-commit.sh ../../.git/hooks/pre-commit
+ln -sf "$SCRIPT_DIR/../.githooks/pre-commit.sh" "$SCRIPT_DIR/../.git/hooks/pre-commit"
 
 echo "Info: Pre-commit hook setup completed."
 
 echo "---"
 
-# .ENV FILES
+# .ENV FILE IN FRONTEND
 rename_frontend_env() {
   local path="$1"
   local env_file="$path/.env"
@@ -47,8 +47,46 @@ rename_frontend_env() {
   fi
 }
 
-rename_frontend_env "$FRONTEND_PATH" "frontend"
-rename_server_properties "$BACKEND_PATH" "server" # TODO
+rename_frontend_config_file() {
+  local path="$1"
+  local env_file="$path/.env"
+  local sample_file="$env_file.sample"
+  local folder="$2"
+
+  if [ ! -f "$env_file" ]; then
+    cp "$sample_file" "$env_file"
+    if [ -f "$env_file" ]; then
+      echo "Info: Successfully renamed .env file in $folder. Now you can add variables."
+    else 
+      echo "Error: Failed to rename .env file in $folder."
+      exit 1
+    fi
+  else
+    echo "Info: .env file is present in $folder."
+  fi
+}
+
+rename_server_config_file() {
+  local path="$1"
+  local env_file="$path/src/main/resources/application.properties"
+  local sample_file="$path/src/main/resources/example.application.properties"
+  local folder="$2"
+
+  if [ ! -f "$env_file" ]; then
+    cp "$sample_file" "$env_file"
+    if [ -f "$env_file" ]; then
+      echo "Info: Successfully renamed .env file in $folder. Now you can add variables."
+    else 
+      echo "Error: Failed to rename .env file in $folder."
+      exit 1
+    fi
+  else
+    echo "Info: application.properties file is present in $folder."
+  fi
+}
+
+rename_frontend_config_file "$FRONTEND_PATH" "frontend"
+rename_server_config_file "$BACKEND_PATH" "server"
 
 echo "---"
 
