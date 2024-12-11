903cd3649d062d7321ae86b59a587659c5486dae
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-11 23:48:22 +0300

    Removed generic algorithm related files

--- server/src/main/java/com/flightcoordinator/server/repository/AlgorithmRepository.java
+++
@@ -1,8 +0,0 @@
-// package com.flightcoordinator.server.repository;
-
-// import org.springframework.data.mongodb.repository.MongoRepository;
-// import org.springframework.stereotype.Repository;
-
-// @Repository
-// public interface AlgorithmRepository extends MongoRepository<String /* Algorithm Model will go here */, String> {
-// }
