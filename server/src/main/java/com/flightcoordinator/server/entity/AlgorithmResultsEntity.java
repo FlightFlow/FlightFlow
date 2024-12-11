06ee647ba1998443f4113ddebed0faf317ccfd35
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-11 23:40:00 +0300

    Added algorithm entities

--- server/src/main/java/com/flightcoordinator/server/entity/AlgorithmResultsEntity.java
+++ server/src/main/java/com/flightcoordinator/server/entity/AlgorithmResultsEntity.java
@@ -1,10 +1,99 @@
 package com.flightcoordinator.server.entity;
 
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import jakarta.validation.constraints.NotBlank;
 
 @Document(collection = "algorithm_results")
 public class AlgorithmResultsEntity {
   @Id
   private String id;
+
+  @Field("route_id")
+  @NotBlank(message = "Route ID cannot be blank")
+  private String routeId;
+
+  @Field("plane_id")
+  @NotBlank(message = "Plane ID cannot be blank")
+  private String planeId;
+
+  @Field("crew_member_ids")
+  @NotBlank(message = "Crew member IDs cannot be blank")
+  private List<String> crewMemberIds;
+
+  @Field("runway_id")
+  @NotBlank(message = "Runway ID cannot be blank")
+  private String runwayId;
+
+  @Field("ground_vehicle_ids")
+  @NotBlank(message = "Ground vehicle IDs cannot be blank")
+  private List<String> groundVehicleIds;
+
+  public AlgorithmResultsEntity() {
+  }
+
+  public AlgorithmResultsEntity(String id, @NotBlank(message = "Route ID cannot be blank") String routeId,
+      @NotBlank(message = "Plane ID cannot be blank") String planeId,
+      @NotBlank(message = "Crew member IDs cannot be blank") List<String> crewMemberIds,
+      @NotBlank(message = "Runway ID cannot be blank") String runwayId,
+      @NotBlank(message = "Ground vehicle IDs cannot be blank") List<String> groundVehicleIds) {
+    this.id = id;
+    this.routeId = routeId;
+    this.planeId = planeId;
+    this.crewMemberIds = crewMemberIds;
+    this.runwayId = runwayId;
+    this.groundVehicleIds = groundVehicleIds;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getRouteId() {
+    return routeId;
+  }
+
+  public void setRouteId(String routeId) {
+    this.routeId = routeId;
+  }
+
+  public String getPlaneId() {
+    return planeId;
+  }
+
+  public void setPlaneId(String planeId) {
+    this.planeId = planeId;
+  }
+
+  public List<String> getCrewMemberIds() {
+    return crewMemberIds;
+  }
+
+  public void setCrewMemberIds(List<String> crewMemberIds) {
+    this.crewMemberIds = crewMemberIds;
+  }
+
+  public String getRunwayId() {
+    return runwayId;
+  }
+
+  public void setRunwayId(String runwayId) {
+    this.runwayId = runwayId;
+  }
+
+  public List<String> getGroundVehicleIds() {
+    return groundVehicleIds;
+  }
+
+  public void setGroundVehicleIds(List<String> groundVehicleIds) {
+    this.groundVehicleIds = groundVehicleIds;
+  }
 }
--- server/src/main/java/com/flightcoordinator/server/entity/AlgorithmRunsEntity.java
+++ server/src/main/java/com/flightcoordinator/server/entity/AlgorithmRunsEntity.java
@@ -1,10 +1,190 @@
 package com.flightcoordinator.server.entity;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import jakarta.validation.constraints.NotBlank;
 
 @Document(collection = "algorithm_runs")
 public class AlgorithmRunsEntity {
   @Id
   private String id;
+
+  @Field("algorithm_name")
+  @NotBlank(message = "Algorithm name cannot be blank")
+  private String algorithmName;
+
+  @Field("start_time")
+  @NotBlank(message = "Start time cannot be blank")
+  private Date startTime;
+
+  @Field("end_time")
+  @NotBlank(message = "End time cannot be blank")
+  private Date endTime;
+
+  @Field("runtime_in_milliseconds")
+  @NotBlank(message = "Runtime in ms cannot be blank")
+  private long runtimeInMilliseconds;
+
+  @Field("parameters")
+  @NotBlank(message = "Parameters cannot be blank")
+  private Map<String, Object> parameters;
+
+  @Field("resources_met")
+  @NotBlank(message = "Resources used cannot be blank")
+  private Map<String, Object> resourcesUsed;
+
+  @Field("constrains_met")
+  @NotBlank(message = "Constraints met cannot be blank")
+  private Map<String, Boolean> constrainsMet;
+
+  @Field("logs")
+  private List<String> logs;
+
+  @Field("is_successful")
+  private boolean isSuccessful;
+
+  @Field("failure_reason")
+  private String failureReason;
+
+  @Field("is_results_saved")
+  private boolean isResultsSaved;
+
+  @Field("result_id")
+  private String resultId;
+
+  public AlgorithmRunsEntity() {
+  }
+
+  public AlgorithmRunsEntity(String id, @NotBlank(message = "Algorithm name cannot be blank") String algorithmName,
+      @NotBlank(message = "Start time cannot be blank") Date startTime,
+      @NotBlank(message = "End time cannot be blank") Date endTime,
+      @NotBlank(message = "Runtime in ms cannot be blank") long runtimeInMilliseconds,
+      @NotBlank(message = "Parameters cannot be blank") Map<String, Object> parameters,
+      @NotBlank(message = "Resources used cannot be blank") Map<String, Object> resourcesUsed,
+      @NotBlank(message = "Constraints met cannot be blank") Map<String, Boolean> constrainsMet, List<String> logs,
+      boolean isSuccessful, String failureReason, boolean isResultsSaved, String resultId) {
+    this.id = id;
+    this.algorithmName = algorithmName;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.runtimeInMilliseconds = runtimeInMilliseconds;
+    this.parameters = parameters;
+    this.resourcesUsed = resourcesUsed;
+    this.constrainsMet = constrainsMet;
+    this.logs = logs;
+    this.isSuccessful = isSuccessful;
+    this.failureReason = failureReason;
+    this.isResultsSaved = isResultsSaved;
+    this.resultId = resultId;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getAlgorithmName() {
+    return algorithmName;
+  }
+
+  public void setAlgorithmName(String algorithmName) {
+    this.algorithmName = algorithmName;
+  }
+
+  public Date getStartTime() {
+    return startTime;
+  }
+
+  public void setStartTime(Date startTime) {
+    this.startTime = startTime;
+  }
+
+  public Date getEndTime() {
+    return endTime;
+  }
+
+  public void setEndTime(Date endTime) {
+    this.endTime = endTime;
+  }
+
+  public long getRuntimeInMilliseconds() {
+    return runtimeInMilliseconds;
+  }
+
+  public void setRuntimeInMilliseconds(long runtimeInMilliseconds) {
+    this.runtimeInMilliseconds = runtimeInMilliseconds;
+  }
+
+  public Map<String, Object> getParameters() {
+    return parameters;
+  }
+
+  public void setParameters(Map<String, Object> parameters) {
+    this.parameters = parameters;
+  }
+
+  public Map<String, Object> getResourcesUsed() {
+    return resourcesUsed;
+  }
+
+  public void setResourcesUsed(Map<String, Object> resourcesUsed) {
+    this.resourcesUsed = resourcesUsed;
+  }
+
+  public Map<String, Boolean> getConstrainsMet() {
+    return constrainsMet;
+  }
+
+  public void setConstrainsMet(Map<String, Boolean> constrainsMet) {
+    this.constrainsMet = constrainsMet;
+  }
+
+  public List<String> getLogs() {
+    return logs;
+  }
+
+  public void setLogs(List<String> logs) {
+    this.logs = logs;
+  }
+
+  public boolean isSuccessful() {
+    return isSuccessful;
+  }
+
+  public void setSuccessful(boolean isSuccessful) {
+    this.isSuccessful = isSuccessful;
+  }
+
+  public String getFailureReason() {
+    return failureReason;
+  }
+
+  public void setFailureReason(String failureReason) {
+    this.failureReason = failureReason;
+  }
+
+  public boolean isResultsSaved() {
+    return isResultsSaved;
+  }
+
+  public void setResultsSaved(boolean isResultsSaved) {
+    this.isResultsSaved = isResultsSaved;
+  }
+
+  public String getResultId() {
+    return resultId;
+  }
+
+  public void setResultId(String resultId) {
+    this.resultId = resultId;
+  }
 }
