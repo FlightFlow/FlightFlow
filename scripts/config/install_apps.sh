fb94dbe0325ad11e3db714302fab77e2c3160c13
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-12 09:52:42 +0300

    Moved links and versions into variables

--- scripts/config/install_apps.sh
+++ scripts/config/install_apps.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+MAVEN_VERSION="apache-maven-3.9.9"
+
+NVM_SOURCE="https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh"
+PNPM_SOURCE="https://get.pnpm.io/install.sh"
+MAVEN_SOURCE="https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/$MAVEN_VERSION.tar.gz"
+
 # NVM AND NODE
 echo "Info: Installing Node Version Manager..."
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+curl -o- "$NVM_SOURCE" | bash
 nvm install 22
 
 echo "Info: Succesfully installed Node Version Manager."
@@ -11,7 +17,7 @@ echo "Info: Succesfully installed Node Version Manager."
 # PNPM
 echo "Info: Installing pnpm..."
 
-curl -fsSL https://get.pnpm.io/install.sh | sh -
+curl -fsSL "$PNPM_SOURCE"| sh -
 
 echo "Info: Successfully installed pnpm."
 
@@ -20,12 +26,12 @@ echo "Info: Installing Maven..."
 
 if ! grep -q 'export PATH=$MAVEN_HOME/bin:$PATH' ~/.bashrc; then
   cd /tmp
-  wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
-  sudo tar -xvzf apache-maven-3.9.9-bin.tar.gz -C /opt
-  sudo rm -rf apache-maven-3.9.9-bin.tar.gz
+  wget "$MAVEN_SOURCE"
+  sudo tar -xvzf "$MAVEN_VERSION-bin.tar.gz" -C /opt
+  sudo rm -rf "$MAVEN_VERSION-bin.tar.gz"
 
   echo "" >> ~/.bashrc
-  echo "export MAVEN_HOME=/opt/apache-maven-3.9.9" >> ~/.bashrc
+  echo "export MAVEN_HOME=/opt/$MAVEN_VERSION" >> ~/.bashrc
   echo 'PATH=$MAVEN_HOME/bin:$PATH' >> ~/.bashrc
 
   source ~/.bashrc
