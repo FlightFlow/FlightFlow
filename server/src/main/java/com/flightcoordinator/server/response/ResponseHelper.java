83f23e1cafd1f206149d188067e1e42180bfe83a
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-13 21:57:40 +0300

    Updated formatting

--- server/src/main/java/com/flightcoordinator/server/response/ResponseHelper.java
+++ server/src/main/java/com/flightcoordinator/server/response/ResponseHelper.java
@@ -3,7 +3,10 @@ package com.flightcoordinator.server.response;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseHelper<T> {
-  public static <T> ResponseEntity<ResponseObject<T>> generateResponse(int status, boolean isSuccess, String message,
+  public static <T> ResponseEntity<ResponseObject<T>> generateResponse(
+      int status,
+      boolean isSuccess,
+      String message,
       T data) {
     return ResponseEntity.status(status).body(new ResponseObject<>(isSuccess, message, data));
   }
