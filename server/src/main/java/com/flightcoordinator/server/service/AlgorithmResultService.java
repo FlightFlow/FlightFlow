d1fc36195c53de7f6133217719d8c61955c2c4cc
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-15 00:12:53 +0300

    Updated services

--- server/src/main/java/com/flightcoordinator/server/service/AlgorithmResultService.java
+++ server/src/main/java/com/flightcoordinator/server/service/AlgorithmResultService.java
@@ -1,12 +1,81 @@
 package com.flightcoordinator.server.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.flightcoordinator.server.entity.AlgorithmResultEntity;
+import com.flightcoordinator.server.exception.AppError;
 import com.flightcoordinator.server.repository.AlgorithmResultRepository;
 
 @Service
 public class AlgorithmResultService {
   @Autowired
   private AlgorithmResultRepository algorithmResultRepository;
+
+  public AlgorithmResultEntity getSingleAlgorithmResultById(String algorithmResultId) {
+    Optional<AlgorithmResultEntity> algorithmResult = algorithmResultRepository.findById(algorithmResultId);
+    return algorithmResult.orElseThrow(() -> new AppError(
+        HttpStatus.NOT_FOUND.getReasonPhrase(),
+        HttpStatus.NOT_FOUND.value()));
+  }
+
+  public List<AlgorithmResultEntity> getMultipleAlgorithmResultsById(List<String> algorithmResultIds) {
+    List<AlgorithmResultEntity> algorithmResults = algorithmResultRepository.findAllById(algorithmResultIds);
+    if (algorithmResults.isEmpty()) {
+      throw new AppError(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND.value());
+    }
+    return algorithmResults;
+  }
+
+  public List<AlgorithmResultEntity> getAllAlgorithmResults() {
+    List<AlgorithmResultEntity> algorithmResults = algorithmResultRepository.findAll();
+    if (algorithmResults.isEmpty()) {
+      throw new AppError(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND.value());
+    }
+    return algorithmResults;
+  }
+
+  public void createAlgorithmResult(AlgorithmResultEntity newAlgorithmResult) {
+    algorithmResultRepository.save(newAlgorithmResult);
+  }
+
+  public void updateAlgorithmResult(String algorithmResultId, AlgorithmResultEntity updatedAlgorithmResult) {
+    AlgorithmResultEntity existingAlgorithmResult = getSingleAlgorithmResultById(algorithmResultId);
+
+    // TODO: add validations
+
+    existingAlgorithmResult.setRoute(updatedAlgorithmResult.getRoute());
+    existingAlgorithmResult.setPlane(updatedAlgorithmResult.getPlane());
+    existingAlgorithmResult.setCrewMembers(updatedAlgorithmResult.getCrewMembers());
+    existingAlgorithmResult.setRunways(updatedAlgorithmResult.getRunways());
+    existingAlgorithmResult.setGroundVehicles(updatedAlgorithmResult.getGroundVehicles());
+
+    algorithmResultRepository.save(existingAlgorithmResult);
+  }
+
+  public void deleteAlgorithmResult(String algorithmResultId) {
+    AlgorithmResultEntity existingAlgorithmResult = getSingleAlgorithmResultById(algorithmResultId);
+    algorithmResultRepository.delete(existingAlgorithmResult);
+  }
+
+  public Boolean doesSingleAlgorithmResultExist(AlgorithmResultEntity algorithmResult) {
+    String algorithmResultId = algorithmResult.getId();
+    Optional<AlgorithmResultEntity> algorithmResultFound = algorithmResultRepository.findById(algorithmResultId);
+    return algorithmResultFound.isPresent();
+  }
+
+  public Boolean doesMultipleAlgorithmResultsExist(List<AlgorithmResultEntity> algorithmResults) {
+    List<String> algorithmResultIds = new ArrayList<>();
+    algorithmResults.forEach(algorithmResult -> algorithmResultIds.add(algorithmResult.getId()));
+    List<AlgorithmResultEntity> algorithmResultsFound = algorithmResultRepository.findAllById(algorithmResultIds);
+    if (algorithmResults.size() != algorithmResultsFound.size()) {
+      return false;
+    }
+    return algorithmResultsFound.isEmpty();
+  }
 }
--- server/src/main/java/com/flightcoordinator/server/service/AlgorithmRunService.java
+++ server/src/main/java/com/flightcoordinator/server/service/AlgorithmRunService.java
@@ -1,12 +1,86 @@
 package com.flightcoordinator.server.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.flightcoordinator.server.entity.AlgorithmRunEntity;
+import com.flightcoordinator.server.exception.AppError;
 import com.flightcoordinator.server.repository.AlgorithmRunRepository;
 
 @Service
 public class AlgorithmRunService {
   @Autowired
   private AlgorithmRunRepository algorithmRunRepository;
+
+  public AlgorithmRunEntity getSingleAlgorithmRunById(String algorithmRunId) {
+    Optional<AlgorithmRunEntity> algorithmRun = algorithmRunRepository.findById(algorithmRunId);
+    return algorithmRun.orElseThrow(() -> new AppError(
+        HttpStatus.NOT_FOUND.getReasonPhrase(),
+        HttpStatus.NOT_FOUND.value()));
+  }
+
+  public List<AlgorithmRunEntity> getMultipleAlgorithmRunsById(List<String> algorithmRunIds) {
+    List<AlgorithmRunEntity> algorithmRuns = algorithmRunRepository.findAllById(algorithmRunIds);
+    if (algorithmRuns.isEmpty()) {
+      throw new AppError(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND.value());
+    }
+    return algorithmRuns;
+  }
+
+  public List<AlgorithmRunEntity> getAllAlgorithmRuns() {
+    List<AlgorithmRunEntity> algorithmRuns = algorithmRunRepository.findAll();
+    if (algorithmRuns.isEmpty()) {
+      throw new AppError(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND.value());
+    }
+    return algorithmRuns;
+  }
+
+  public void createAlgorithmRun(AlgorithmRunEntity newAlgorithmRun) {
+    algorithmRunRepository.save(newAlgorithmRun);
+  }
+
+  public void updateAlgorithmRun(String algorithmRunId, AlgorithmRunEntity updatedAlgorithmRun) {
+    AlgorithmRunEntity existingAlgorithmRun = getSingleAlgorithmRunById(algorithmRunId);
+
+    existingAlgorithmRun.setAlgorithmName(updatedAlgorithmRun.getAlgorithmName());
+    existingAlgorithmRun.setStartTime(updatedAlgorithmRun.getStartTime());
+    existingAlgorithmRun.setEndTime(updatedAlgorithmRun.getEndTime());
+    existingAlgorithmRun.setRuntimeInMilliseconds(updatedAlgorithmRun.getRuntimeInMilliseconds());
+    existingAlgorithmRun.setParameters(updatedAlgorithmRun.getParameters());
+    existingAlgorithmRun.setResourcesUsed(updatedAlgorithmRun.getResourcesUsed());
+    existingAlgorithmRun.setConstrainsMet(updatedAlgorithmRun.getConstrainsMet());
+    existingAlgorithmRun.setLogs(updatedAlgorithmRun.getLogs());
+    existingAlgorithmRun.setSuccessful(updatedAlgorithmRun.isSuccessful());
+    existingAlgorithmRun.setFailureReason(updatedAlgorithmRun.getFailureReason());
+    existingAlgorithmRun.setResultsSaved(updatedAlgorithmRun.isResultsSaved());
+    existingAlgorithmRun.setResult(updatedAlgorithmRun.getResult());
+
+    algorithmRunRepository.save(existingAlgorithmRun);
+  }
+
+  public void deleteAlgorithmRun(String algorithmRunId) {
+    AlgorithmRunEntity existingAlgorithmRun = getSingleAlgorithmRunById(algorithmRunId);
+    algorithmRunRepository.delete(existingAlgorithmRun);
+  }
+
+  public Boolean doesSingleAlgorithmRunExist(AlgorithmRunEntity algorithmRun) {
+    String algorithmRunId = algorithmRun.getId();
+    Optional<AlgorithmRunEntity> algorithmRunFound = algorithmRunRepository.findById(algorithmRunId);
+    return algorithmRunFound.isPresent();
+  }
+
+  public Boolean doesMultipleAlgorithmRunsExist(List<AlgorithmRunEntity> algorithmRuns) {
+    List<String> algorithmRunIds = new ArrayList<>();
+    algorithmRuns.forEach(algorithmRun -> algorithmRunIds.add(algorithmRun.getId()));
+    List<AlgorithmRunEntity> algorithmRunsFound = algorithmRunRepository.findAllById(algorithmRunIds);
+    if (algorithmRuns.size() != algorithmRunsFound.size()) {
+      return false;
+    }
+    return algorithmRunsFound.isEmpty();
+  }
 }
