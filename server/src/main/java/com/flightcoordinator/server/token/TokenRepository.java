9f2498b322e9d44683556f2818d753d0e5bc6adc
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-15 00:13:06 +0300

    Updated token repository and service

--- server/src/main/java/com/flightcoordinator/server/token/TokenRepository.java
+++ server/src/main/java/com/flightcoordinator/server/token/TokenRepository.java
@@ -1,9 +1,12 @@
 package com.flightcoordinator.server.token;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TokenRepository extends JpaRepository<TokenEntity, String> {
   Optional<TokenEntity> findByToken(String token);
+
+  List<TokenEntity> findByAssociatedUser(String userId);
 }
