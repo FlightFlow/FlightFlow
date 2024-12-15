9f0ac9edabd14fcc339271158494170defa70536
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-15 20:22:43 +0300

    Added flight controller and service

--- server/src/main/java/com/flightcoordinator/server/controller/FlightController.java
+++ server/src/main/java/com/flightcoordinator/server/controller/FlightController.java
@@ -1,11 +1,25 @@
 package com.flightcoordinator.server.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flightcoordinator.server.entity.FlightEntity;
+import com.flightcoordinator.server.response.ResponseHelper;
+import com.flightcoordinator.server.response.ResponseObject;
 import com.flightcoordinator.server.service.FlightService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -14,4 +28,41 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class FlightController {
   @Autowired
   private FlightService flightService;
+
+  @GetMapping("/getById/{flightId}")
+  @Operation(summary = "Get a flight by id", description = "Retrieve the details of a spesific flight using it's ID.")
+  public ResponseEntity<ResponseObject<FlightEntity>> getFlightById(@PathVariable String flightId) {
+    FlightEntity flight = flightService.getSingleFlightById(flightId);
+    return ResponseHelper.generateResponse(HttpStatus.OK.value(), true, HttpStatus.OK.getReasonPhrase(), flight);
+  }
+
+  @GetMapping("/getAll")
+  @Operation(summary = "Get all the flights", description = "Retrieve the details of all flights.")
+  public ResponseEntity<ResponseObject<List<FlightEntity>>> getAllFlights() {
+    List<FlightEntity> flights = flightService.getAllFlights();
+    return ResponseHelper.generateResponse(HttpStatus.OK.value(), true, HttpStatus.OK.getReasonPhrase(), flights);
+  }
+
+  @PostMapping("/create")
+  @Operation(summary = "Create a new flight", description = "Create a new flight.")
+  public ResponseEntity<ResponseObject<Object>> createFlight(@RequestBody FlightEntity flight) {
+    flightService.createFlight(flight);
+    return ResponseHelper.generateResponse(HttpStatus.CREATED.value(), true, HttpStatus.CREATED.getReasonPhrase(),
+        null);
+  }
+
+  @PatchMapping("/update/{flightId}")
+  @Operation(summary = "Update a flight", description = "Update an existing flight.")
+  public ResponseEntity<ResponseObject<Object>> updateFlight(@PathVariable String flightId,
+      @RequestBody FlightEntity flight) {
+    flightService.updateFlight(flightId, flight);
+    return ResponseHelper.generateResponse(HttpStatus.OK.value(), true, HttpStatus.OK.getReasonPhrase(), null);
+  }
+
+  @DeleteMapping("/delete/{flightId}")
+  @Operation(summary = "Delete a flight", description = "Delete an existing flight.")
+  public ResponseEntity<ResponseObject<Object>> deleteFlight(@PathVariable String flightId) {
+    flightService.deleteFlight(flightId);
+    return ResponseHelper.generateResponse(HttpStatus.OK.value(), true, HttpStatus.OK.getReasonPhrase(), null);
+  }
 }
--- server/src/main/java/com/flightcoordinator/server/service/FlightService.java
+++ server/src/main/java/com/flightcoordinator/server/service/FlightService.java
@@ -1,12 +1,89 @@
 package com.flightcoordinator.server.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.flightcoordinator.server.entity.FlightEntity;
+import com.flightcoordinator.server.exception.AppError;
 import com.flightcoordinator.server.repository.FlightRepository;
 
 @Service
 public class FlightService {
   @Autowired
   private FlightRepository flightRepository;
+
+  @Autowired
+  private RouteService routeService;
+
+  public FlightEntity getSingleFlightById(String flightId) {
+    Optional<FlightEntity> flight = flightRepository.findById(flightId);
+    return flight.orElseThrow(() -> new AppError(
+        HttpStatus.NOT_FOUND.getReasonPhrase(),
+        HttpStatus.NOT_FOUND.value()));
+  }
+
+  public List<FlightEntity> getMultipleFlightsById(List<String> flightIds) {
+    List<FlightEntity> flights = flightRepository.findAllById(flightIds);
+    if (flights.isEmpty()) {
+      throw new AppError(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND.value());
+    }
+    return flights;
+  }
+
+  public List<FlightEntity> getAllFlights() {
+    List<FlightEntity> flights = flightRepository.findAll();
+    if (flights.isEmpty()) {
+      throw new AppError(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND.value());
+    }
+    return flights;
+  }
+
+  public void createFlight(FlightEntity newFlight) {
+    Boolean doesRouteExist = routeService.doesSingleRouteExist(newFlight.getFlightRoute());
+    if (!doesRouteExist) {
+      throw new AppError(HttpStatus.BAD_REQUEST.getReasonPhrase(), HttpStatus.BAD_REQUEST.value());
+    }
+
+    flightRepository.save(newFlight);
+  }
+
+  public void updateFlight(String flightId, FlightEntity updatedFlight) {
+    FlightEntity existingFlight = getSingleFlightById(flightId);
+
+    Boolean doesRouteExist = routeService.doesSingleRouteExist(existingFlight.getFlightRoute());
+    if (!doesRouteExist) {
+      throw new AppError(HttpStatus.BAD_REQUEST.getReasonPhrase(), HttpStatus.BAD_REQUEST.value());
+    }
+
+    existingFlight.setPassengerCount(updatedFlight.getPassengerCount());
+    existingFlight.setFlightRoute(updatedFlight.getFlightRoute());
+
+    flightRepository.save(existingFlight);
+  }
+
+  public void deleteFlight(String flightId) {
+    FlightEntity existingFlight = getSingleFlightById(flightId);
+    flightRepository.delete(existingFlight);
+  }
+
+  public Boolean doesSingleFlightExist(FlightEntity flight) {
+    String flightId = flight.getId();
+    Optional<FlightEntity> flightFound = flightRepository.findById(flightId);
+    return flightFound.isPresent();
+  }
+
+  public Boolean doesMultipleFlightsExist(List<FlightEntity> flights) {
+    List<String> flightIds = new ArrayList<>();
+    flights.forEach(flight -> flightIds.add(flight.getId()));
+    List<FlightEntity> flightsFound = flightRepository.findAllById(flightIds);
+    if (flights.size() != flightsFound.size()) {
+      return false;
+    }
+    return flightsFound.isEmpty();
+  }
 }
