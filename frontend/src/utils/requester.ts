eceaec92e5a95ca89de167b60dfa029391bba92a
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-15 18:14:43 +0300

    Added token rotation handler to the requester and made url generator method more flexible by adding port as environment variable

--- frontend/src/utils/requester.ts
+++ frontend/src/utils/requester.ts
@@ -9,31 +9,31 @@ import axios, {
 import i18next from "i18next";
 
 const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
-const POSSIBLE_STATUS_CODES = [200, 201, 400, 401, 404, 409, 500];
+const BACKEND_PORT = import.meta.env.VITE_APP_BACKEND_PORT;
+const BACKEND_API_VERSION = "v1";
+
+const POSSIBLE_STATUS_CODES = [200, 201, 400, 401, 404, 409, 500]; // FIXME
 
 class Requester {
-  private protocol: string;
+  private protocol: UtilTypes.RequesterBaseTypes.RequestProtocols = "http";
   private baseURL: string = BACKEND_URL.replace(/\/$/, "");
+  private port: string = BACKEND_PORT;
   private endpoint: UtilTypes.RequesterBaseTypes.EndpointProps;
   private method: string;
   private contentType: string = "application/json";
   private headers: RawAxiosRequestHeaders;
-  private accessToken: string;
-  private includeCookies: boolean;
+  private includeCookies: boolean = true;
   private query?: Record<string, string>;
   private payload: object;
   private responseLanguage: string = i18next.language;
 
   constructor(requesterConfig: UtilTypes.RequesterBaseTypes.RequesterConfigParams) {
-    this.protocol = requesterConfig.protocol ?? "http";
     this.endpoint = requesterConfig.endpoint;
     this.method = requesterConfig.method;
-    this.accessToken = requesterConfig.accessToken ?? "";
     this.includeCookies = requesterConfig.includeCookies ?? false;
     this.headers = {
       ...requesterConfig.headers,
       "Content-Type": this.contentType,
-      ...(requesterConfig.accessToken && { Authorization: `Bearer ${this.accessToken}` }),
       "Accept-Language": this.responseLanguage,
     };
     this.payload = requesterConfig.payload;
@@ -51,18 +51,36 @@ class Requester {
   }
 
   private generateURL(): string {
-    const urlString: string = `${this.protocol}://${this.baseURL}/`;
+    const urlString: string = `${this.protocol}://${this.baseURL}:${this.port}/api/${BACKEND_API_VERSION}`;
     const endpointString: string = this.generateEndpoint();
-    const queryString: string = this.query ? new URLSearchParams(this.query).toString() : "";
-    return `${urlString}${endpointString}?${queryString}`;
+    const queryString: string = `?${this.query ? new URLSearchParams(this.query).toString() : ""}`;
+    return `${urlString}${endpointString}${queryString}`;
   }
 
-  // TODO: add a token rotation handler
+  private generateBaseURL(): string {
+    return `${this.protocol}://${this.baseURL}:${this.port}/api/${BACKEND_API_VERSION}`;
+  }
 
-  async sendRequest<
-    TResponseData = null,
-    TRequestPayload = UtilTypes.RequesterBaseTypes.RequesterConfigParams,
-  >(): Promise<GlobalTypes.ServerResponseParams<TResponseData>> {
+  private async getNewAccessToken(): Promise<boolean> {
+    const tokenEndpoint: string = `${this.generateBaseURL()}/user/auth/newAccessToken`;
+    const axiosInstance = axios.create({ baseURL: tokenEndpoint });
+    const axiosConfig: AxiosRequestConfig = {
+      url: tokenEndpoint,
+      method: "POST",
+      withCredentials: true,
+    };
+    const response: AxiosResponse<GlobalTypes.ServerResponseParams, AxiosResponse> =
+      await axiosInstance.request(axiosConfig);
+
+    if (response.data.isSuccess) {
+      return true;
+    }
+    return false;
+  }
+
+  async sendRequest<TResponseData = null, TRequestPayload = null>(): Promise<
+    GlobalTypes.ServerResponseParams<TResponseData>
+  > {
     const axiosInstance = axios.create({ baseURL: this.generateURL() });
     axiosInstance.interceptors.response.use(
       (response: AxiosResponse<TResponseData, TRequestPayload>) => {
@@ -89,7 +107,21 @@ class Requester {
       GlobalTypes.ServerResponseParams<TResponseData>,
       AxiosResponse<TResponseData, TRequestPayload>
     >(axiosConfig);
-    return response.data as GlobalTypes.ServerResponseParams<TResponseData>;
+
+    const responseData = response.data as GlobalTypes.ServerResponseParams<TResponseData>;
+
+    if (!responseData.isSuccess && responseData.responseMessage == "Expired token") {
+      const tryGetNewAccessToken: boolean = await this.getNewAccessToken();
+
+      if (!tryGetNewAccessToken) {
+        return {
+          isSuccess: false,
+          responseMessage: "Your session is expired. Please login again.",
+          responseData: undefined,
+        } satisfies GlobalTypes.ServerResponseParams;
+      }
+    }
+    return responseData;
   }
 }
 
