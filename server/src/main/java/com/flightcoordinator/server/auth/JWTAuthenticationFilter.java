07083d42a0165f03f314cf69c0bf9882eaf2a094
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-15 18:15:26 +0300

    Renamed JWTFilter to JWTAuthenticationFilter

--- server/src/main/java/com/flightcoordinator/server/auth/JWTAuthenticationFilter.java
+++ server/src/main/java/com/flightcoordinator/server/auth/JWTAuthenticationFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,12 +12,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.flightcoordinator.server.exception.AppError;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JWTFilter extends OncePerRequestFilter {
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
   @Autowired
   private JWTService jwtService;
 
@@ -49,6 +52,8 @@ public class JWTFilter extends OncePerRequestFilter {
             userDetails.getAuthorities());
         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      } else {
+        throw new AppError("Expired Token", HttpStatus.UNAUTHORIZED.value());
       }
     }
     filterChain.doFilter(request, response);
