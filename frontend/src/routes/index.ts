9be5754a92be404fed7bd255e6e41fc95b943411
Çağan Seyrek <caganseyrek@outlook.com.tr>
2024-12-20 18:43:16 +0300

    Added index for exporting routes

---
+++ frontend/src/routes/index.ts
@@ -0,0 +1,10 @@
+// Public Routes
+export { default as LoginPage } from "./public/LoginPage";
+export { default as RegisterPage } from "./public/RegisterPage";
+
+// Private Routes
+export { default as AirportPage } from "./private/AirportsPage";
+
+// Misc Routes
+export { default as ErrorPage } from "./misc/ErrorPage";
+export { default as NotFoundPage } from "./misc/NotFoundPage";
