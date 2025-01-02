6f08efb79004469023fa79bd3f5c3a74ca0be450
Ezgi Nur Elbistan <ezginurelbistan@gmail.com>
2025-01-02 22:30:36 +0300

    Missing hooks have been completed. Algorithm run, algorithm result and flight sections have been added to the resource section.

---
+++ frontend/src/hooks/flight/useFlightDeleteMutation.ts
@@ -0,0 +1,29 @@
+import ResourceTypes from "@/types/resource";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import Requester from "@/utils/requester";
+
+import useAccessToken from "../useAccessToken";
+
+const useFlightDeleteMutation = () => {
+  const queryClient = useQueryClient();
+  const accessToken = useAccessToken();
+  const deleteFlight = useMutation({
+    mutationKey: ["deleteFlightMutation"],
+    mutationFn: async (flightDeleteData: ResourceTypes.Flight.Mutations.DeleteMutationParams) => {
+      const response = await new Requester()
+        .setConfig({
+          method: "DELETE",
+          endpoint: { controller: "flight", action: "delete" },
+          payload: flightDeleteData,
+          accessToken: accessToken,
+        })
+        .sendRequest();
+      return response;
+    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["flightQuery"] }),
+  });
+  return deleteFlight;
+};
+
+export default useFlightDeleteMutation;
